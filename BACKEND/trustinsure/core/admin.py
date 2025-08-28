6d3d3906ddd8a887e5be42269db89986d074b685
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-08-28 14:31:37 +0300

    registered the models in my admin

--- BACKEND/trustinsure/core/admin.py
+++ BACKEND/trustinsure/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Policy, Claim, Payment
 
 # Register your models here.
+
+admin.site.register(Policy)
+admin.site.register(Claim)
+admin.site.register(Payment)
