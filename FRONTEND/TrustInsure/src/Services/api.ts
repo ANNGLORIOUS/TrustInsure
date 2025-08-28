570299577a07c354dfe427e2f5b44781806c9f94
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-08-28 12:05:23 +0300

    created the pages, components and services folder

---
+++ FRONTEND/TrustInsure/src/Services/api.ts
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// const API_URL = "http://127.0.0.1:8000/api"; // Django backend
+
+// export const getPolicies = () => axios.get(`${API_URL}/policies/`);
+// export const getClaims = () => axios.get(`${API_URL}/claims/`);
+// export const getPayments = () => axios.get(`${API_URL}/payments/`);
+
+// export const createPolicy = (data: any) => axios.post(`${API_URL}/policies/`, data);
+// export const createClaim = (data: any) => axios.post(`${API_URL}/claims/`, data);
+// export const createPayment = (data: any) => axios.post(`${API_URL}/payments/`, data);
